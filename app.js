5b7becea5720d1247aa4b12ef1e13af81c23311a
Vitali Maksimov <vitamak@greeny.cs.tlu.ee>
2016-02-10 11:05:20 +0200

    mouseEvent

--- app.js
+++ app.js
@@ -8,9 +8,31 @@
 		}
 		Moosipurk.instance = this; // this viitam 
 			
-		console.log('moosipugri sees');
-			
+		console.log(this);
+		//consloe.log('moosipurk')
+		//panen rakk tööma
+		this.init();
 	};
+		
+		//koik moosiporgi funct-d tulevad siia sees
+	Moosipurk.prototype = {
+		init: function(){
+			console.log('rakendus käivetus');
+			//Siia tuleb esialgne Loogika
+			//hakka kuulama hiirklõpse
+			this.bindMouseEvents();
+		},
+		
+		bindMouseEvents: function(){
+			document.querySelector('.add-new-jar').addEventListener('click', this.addNewClick.bind(this));
+		},
+		addNewClick: function(event){
+			console.log(event);
+			
+		}
+		
+	};
+		
 		
 	window.onload = function(){
 		var app = new Moosipurk();
